29f034e850361439d259d67db77110732c22b242
VRMX2 <lehcengrissi@gmail.com>
2025-09-24 13:31:24 -0700

    new commit

---
+++ server/src/server.js
@@ -0,0 +1,20 @@
+import fastify from 'fastify';
+import 'dotenv/config';
+import { connectDB } from './config/connect.js';
+import { PORT } from './config/config.js';
+
+
+const start = async() => {
+	await connectDB(process.env.MONGO_URI);
+	const app = fastify();
+
+	app.listen({port: PORT, host: '0.0.0.0'}, (err, addr) => {
+		if(err){
+			console.log(err);
+		}else{
+			console.log(`Grocery App running on http://localhost:${PORT}`);
+		}
+	})
+}
+
+start();
